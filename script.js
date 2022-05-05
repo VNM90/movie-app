31a8bb91e1e9722a92f672ff270e84c2819f0aaf
VNM90 <artek547@gmail.com>
2022-05-06 00:49:16 +0200

    functionality and styling added

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 const baseUrl = "https://w-strapi-movies-app-9wxhf.ondigitalocean.app/api/movies";
-const modal = document.querySelector(".modal-movie")
+const movieForm = document.querySelector(".movie-form");
+let movieTitleArray = [];
 
 // Defining async function
 async function getData(url) {
 
     const loadingArea = document.querySelector(".loading")
+    const response = await fetch(url);
 
+    // Storing data in form of JSON
+    const data = await response.json();
+
+    if(loadingArea) {
     // Storing response
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`Http error: ${response.status}`);
+        try {
+            if (!response.ok) {
+                throw new Error(`Http error: ${response.status}`);
+            }
+            if (response) {
+                loadingArea.style.display = 'none';
+            }
+            showData(data);
         }
-        // Storing data in form of JSON
-        const data = await response.json();
-
-        if (response) {
-            loadingArea.style.display = 'none';
+        catch (error) {
+            loadingArea.innerHTML = error;
         }
-        showData(data);
     }
-    catch (error) {
-        loadingArea.innerHTML= error;
+    if((movieForm) || (loadingArea)) {
+        for (let movie of data.data) {
+            movieTitleArray.push(movie.attributes.title)
+        }
+        return movieTitleArray;
     }
 }
 
@@ -38,37 +47,108 @@ async function removeMovie(id, url= baseUrl) {
     await getData(baseUrl);
 }
 
-
 // Calling async function
 getData(baseUrl);
 
-function showModal(movie) {
-        console.log(movie)
-}
 
+// Modal
+function showModalMain(id, title, description, author, available, year){
+
+    const modal = document.querySelector(".modal-movie");
+
+        const dialog = 
+        
+        `<div>
+        <p>${id}<p>
+        <p>${title}<p>
+        <p>${description}<p>
+        <p>${author}<p>
+        <p>${available}<p>
+        <p>${year}<p>
+        <button class="btn btn-cancel">Close</button>
+        </div>`
+
+            modal.innerHTML = dialog
+
+            document.querySelector(".btn-cancel").addEventListener('click', () => {
+                modal.close()
+            })
+        modal.showModal()
+    }
+ 
 
 // Function to define innerHTML for HTML table
 function showData(data) {
-    let tab = ``;
+
+    const searchInput = document.querySelector(".search-input");
+    const searchButton = document.querySelector(".btn-search")
+    let movieTab = ``;
 
     // Loop to access all rows
     for (let movie of data.data) {
-        console.log(movie)
         const movieAvailability = movie.attributes.available ? "Available" : "Not available";
-        tab +=
+        movieTab +=
+
             `<table class="movie-table">
-             <tr><td class="movie-id">ID: ${movie.id}</td></tr>
-             <tr><td class="movie-title">${movie.attributes.title}</td></tr>
-             <tr><td class="movie-description">${movie.attributes.description}</td></tr>
-             <tr><td class="movie-author">${movie.attributes.author}</td></tr>
-             <tr><td class="movie-availability">${movieAvailability}</td></tr>
-             <tr><td><button onclick="removeMovie(${movie.id})">DELETE</button></tr></td>
-             <tr><td><button class="button-modal" onclick="showModal(${movie.attributes.title})">DETAILS</button></tr></td>
+             <tr><td class="movie movie-id">ID: ${movie.id}</td></tr>
+             <tr><td class="movie movie-title">${movie.attributes.title}</td></tr>
+             <tr><td class="movie movie-description">${movie.attributes.description}</td></tr>
+             <tr><td class="movie movie-author">${movie.attributes.author}</td></tr>
+             <tr><td class="movie movie-availability">${movieAvailability}</td></tr>
+             <tr><td><button class="btn btn-primary" onclick="removeMovie(${movie.id})">DELETE</button></tr></td>
+             <tr><td><button class="btn btn-secondary" onclick="showModalMain(${movie.id},'${movie.attributes.title}','${movie.attributes.description}','${movie.attributes.author}', '${movieAvailability}', '${movie.attributes.year}')">DETAILS</button></tr></td>
              </table>`
+
     }
-    // Setting innerHTML as tab variable
-    document.querySelector(".div-movies").innerHTML = tab;
+    // Setting innerHTML as movieTab variable
+    document.querySelector(".div-movies").innerHTML = movieTab;
 }
 
 
-// <tr><td><button onclick="(() => { console.log(JSON.stringify(${movie.id})})()">DETAILS</button></tr></td>
+// Form
+if(movieForm) {
+    movieForm.addEventListener("submit", async(event) => {
+        event.preventDefault();
+        const movieTitle = document.querySelector(".title").value;
+        const movieAuthor = document.querySelector(".author").value;
+        const movieDescription = document.querySelector(".description").value;
+        const movieAvailability = document.querySelector('input[name="available"]:checked').value;
+        const movieYear = document.querySelector("#date").value;
+        let counter = 0;
+      
+        const attributes = {
+            data: {
+                title: movieTitle,
+                description: movieDescription,
+                author: movieAuthor,
+                available: movieAvailability,
+                year: movieYear
+            } }
+
+        for (title of movieTitleArray) {
+            counter += 1; 
+            if (movieTitle.trim().toUpperCase() == title.trim().toUpperCase()) {
+                alert("SAME TITLE")
+                break;
+            }
+            else if((counter == movieTitleArray.length) || (movieTitleArray.length == 0)){
+                try {
+                    const response = await fetch(baseUrl, {
+                        method: "POST",
+                        headers: {'Content-Type': 'application/json'},
+                        body: JSON.stringify(attributes)
+                    })
+                    await response.json()
+                    alert("SUCCESS")
+                }
+                catch (error) {
+                    alert(error)
+                }
+            movieForm.reset()
+            }
+        }
+    })
+
+}
+
+
